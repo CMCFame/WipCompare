cc2b6fad8ca95d5487aad0d6639d7c20432d9483
CMCFame <jetronico@gmail.com>
2024-07-08 09:25:12 -0600

    Update confiwip.py

--- confiwip.py
+++ confiwip.py
@@ -4,7 +4,68 @@ from typing import Tuple, List, Dict
 import json
 import os
 
-# ... (previous functions remain the same)
+def extract_acronym(file_name: str) -> str:
+    if 'PROD' in file_name:
+        return 'PROD'
+    elif 'QA' in file_name:
+        return 'QA'
+    else:
+        return 'UNKNOWN'
+
+def find_id_column(df: pd.DataFrame) -> str:
+    for col in df.columns:
+        if "Id" in col:
+            return col
+    raise ValueError("No column containing 'Id' found")
+
+@st.cache_data
+def load_and_prepare_dataframe(file_path: str, header_row: int) -> pd.DataFrame:
+    df = pd.read_csv(file_path, header=header_row)
+    id_col = find_id_column(df)
+    df.set_index(id_col, inplace=True)
+    return df
+
+def align_dataframes(df1: pd.DataFrame, df2: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    return df1.align(df2, join='outer', axis=0, fill_value='')
+
+def create_diff_mask(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return df1 != df2
+
+def combine_differences(df1: pd.DataFrame, df2: pd.DataFrame, diff_mask: pd.DataFrame, acronym1: str, acronym2: str) -> pd.DataFrame:
+    combined_values = []
+    for i in range(len(diff_mask)):
+        row_combined = []
+        for col in diff_mask.columns:
+            if pd.isna(df1.iloc[i][col]) and not pd.isna(df2.iloc[i][col]):
+                row_combined.append(f' / {acronym2}: {df2.iloc[i][col]}')
+            elif not pd.isna(df1.iloc[i][col]) and pd.isna(df2.iloc[i][col]):
+                row_combined.append(f'{acronym1}: {df1.iloc[i][col]} / ')
+            elif not pd.isna(df1.iloc[i][col]) and not pd.isna(df2.iloc[i][col]) and (df1.iloc[i][col] != df2.iloc[i][col]):
+                row_combined.append(f'{acronym1}: {df1.iloc[i][col]} / {acronym2}: {df2.iloc[i][col]}')
+            else:
+                row_combined.append('')
+        combined_values.append(row_combined)
+    
+    combined_values_df = pd.DataFrame(combined_values, columns=diff_mask.columns, index=diff_mask.index)
+    combined_values_df.reset_index(inplace=True)
+    return combined_values_df
+
+def filter_empty_rows(df: pd.DataFrame) -> pd.DataFrame:
+    return df[df.apply(lambda row: row.drop(labels=[df.columns[0]]).astype(str).str.strip().any(), axis=1)]
+
+def compare_csv_files(file_path_1: str, file_path_2: str, acronym1: str, acronym2: str, header_row: int, columns_to_compare: List[str]) -> pd.DataFrame:
+    df1 = load_and_prepare_dataframe(file_path_1, header_row)
+    df2 = load_and_prepare_dataframe(file_path_2, header_row)
+    
+    if columns_to_compare:
+        df1 = df1[columns_to_compare]
+        df2 = df2[columns_to_compare]
+    
+    df1_aligned, df2_aligned = align_dataframes(df1, df2)
+    diff_mask = create_diff_mask(df1_aligned, df2_aligned)
+    
+    combined_values_df = combine_differences(df1_aligned, df2_aligned, diff_mask, acronym1, acronym2)
+    return combined_values_df
 
 def save_config(config: Dict[str, any]):
     with open('comparison_config.json', 'w') as f:
